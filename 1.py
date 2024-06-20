af0266807616a52634d47e7f2bac9797c93bec35
Raman Pazniakou <159067610+RomanPoznyakov@users.noreply.github.com>
2024-06-20 15:49:38 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,7 @@
+
+name = "Рома"
+age = 17
+city = "Москва"
+print("Меня зовут:", name)
+print("Мне", age, "лет")
+print("Я живу в городе", city)
---
+++ 3.py
@@ -0,0 +1,2 @@
+favorite_color = "синий"
+print("Мой любимый цвет:", favorite_color)
